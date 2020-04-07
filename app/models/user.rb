b2c8d9326b0b5fcf9e38778791f5b8b5112868ae
github username <github email address>
2020-04-07 16:25:07 -0600

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :name, :username, :email, :password, presence: {message: "%{attribute} cannont be blank."}
+    validates :username, length: { minimum: 6, maximum: 12, message: "%{attribute} must be between 6 and 12 characters long." }
+    validates :username, uniqueness: {message: "%{attribute}s must be unique, and %{value} has already been taken."}
+
+
+end
